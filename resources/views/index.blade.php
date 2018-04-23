8d142e060c9a7cc1b0684bc42838d575dbaaab15
Яковлев Алексей <yacovlef@gmail.com>
2018-04-23 22:02:00 +0300

    Доработан главный раздел

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,29 +2,34 @@
 
 @section('content')
   <div class="row justify-content-center">
-    <div class="col-auto col-sm-8">
-      <table class="table table-hover mt-5">
+    <div class="col-auto col-sm-10 col-md-8">
+      <table class="table mt-3">
         <thead>
           <tr>
             <th scope="col" class="border-0"></th>
-            <th scope="col" colspan="2" class="text-center border-0">Лучшая цена</th>
+            <th scope="col" colspan="2" class="text-center border-0 bg-light">Лучшая цена</th>
             <th scope="col" class="border-0"></th>
           </tr>
         </thead>
         <tbody>
           @foreach ($domains as $domain)
-            <tr>
-              <th scope="row">{{ $domain->name }}</th>
-              <td class="text-center">{{ $domain->prices()->orderBy('price')->first()->price }} руб.</td>
-              <td class="text-center"><img src="{{ Storage::url($domain->prices()->orderBy('price')->first()->registrar->logo) }}" height="20" alt="registrar_logo"> {{ $domain->prices()->orderBy('price')->first()->registrar->name }}</td>
-              <td class="text-right">+ {{ $domain->prices()->count() }} цен(ы)</td>
-            </tr>
+            @if ($domain->prices_count > 0)
+              <tr>
+                <th scope="row"><a href="" class="text-dark">{{ $domain->name }}</a></th>
+                <td class="text-center bg-light">{{ $domain->prices()->orderBy('price')->first()->price }} руб.</td>
+                <td class="text-center bg-light"><img src="{{ Storage::url($domain->prices()->orderBy('price')->first()->registrar->logo) }}" height="20" alt="registrar_logo"> <a href="" class="text-dark">{{ $domain->prices()->orderBy('price')->first()->registrar->name }}</a></td>
+                <td class="text-right"><a href="" class="text-dark">+ {{ $domain->prices_count }} цен(ы)</a></td>
+              </tr>
+            @endif
           @endforeach
         </tbody>
       </table>
 
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-auto">
       {{ $domains->links() }}
     </div>
   </div>
-
 @endsection
