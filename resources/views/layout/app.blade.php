65d85fa44a8e4c0b71dbb3c7d25ca2c59e5cb236
Яковлев Алексей <yacovlef@gmail.com>
2018-04-29 12:38:06 +0300

    Доработаны сетки разделов публичной части

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,10 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=0.6, shrink-to-fit=no">
 
-    <!-- Optional CSS -->
-    <!-- Bootstrap CSS, style -->
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
 
     <title>{{ env('APP_NAME') }}@yield('title')</title>
   </head>
