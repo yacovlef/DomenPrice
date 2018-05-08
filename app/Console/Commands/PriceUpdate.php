630b38934b67347f3a17b3d8acd1f45d89ee9a93
Яковлев Алексей <yacovlef@gmail.com>
2018-05-08 13:01:29 +0300

    Доработан механизм обновления цен

--- app/Console/Commands/PriceUpdate.php
+++ app/Console/Commands/PriceUpdate.php
@@ -1,26 +1,26 @@
 <?php
 
-namespace App\Console\Commands\Registrar;
+namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
 use Symfony\Component\DomCrawler\Crawler;
 
-class Reg extends Command
+class PriceUpdate extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'registrar:reg';
+    protected $signature = 'price:update';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Обновление цен по регистратору REG';
+    protected $description = 'Обновление цен';
 
     /**
      * Create a new command instance.
@@ -56,7 +56,5 @@ class Reg extends Command
 
         $this->line('|' . $name . '|' . $price . '|');
       }
-
     }
-
 }
