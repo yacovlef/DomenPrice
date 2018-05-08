68de3d3e7ae071c3d8dc3db76ee3761d85561ed4
Яковлев Алексей <yacovlef@gmail.com>
2018-05-08 15:57:00 +0300

    Доработан механизм обновления цен

--- app/Console/Commands/PriceUpdate.php
+++ app/Console/Commands/PriceUpdate.php
@@ -4,7 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-use Symfony\Component\DomCrawler\Crawler;
+use GuzzleHttp\Client;
+
+use App\Domain;
 
 class PriceUpdate extends Command
 {
@@ -39,22 +41,15 @@ class PriceUpdate extends Command
      */
     public function handle()
     {
-      $link = 'https://www.reg.ru/domain/new/';
+      $client = new Client();
 
-      $html = file_get_contents($link);
+      $result = $client->request('POST', 'https://www.reg.ru/domain/new/check_queue', [
+        'query' => [
+          'ru' => 1,
+          'domains' => 'parsedomainprice.com',
+        ]
+      ])->getBody();
 
-      $crawler = new Crawler($html);
-
-      $domains = $crawler->filter('#registration > div.b-page__content-wrapper.b-page__content-wrapper_style_indent.l-margin_bottom-normal.b-domain-form__tlds > div.b-tabs.b-tabs_size_medium.b-tabs_type_horizontal.b-tabs_color_default.b-tabs_style_title-line > div.b-tabs__content > div.b-tabs__item-content.b-tabs__item-content_state_current > div > dl > dd > div > div > ul > li')
-      ->each(function (Crawler $node) {
-        return $node;
-      });
-
-      foreach ($domains as $domain) {
-        $name = $domain->filter('label > span.b-table-tlds__name-wrapper > span > span > span.b-checkbox__name')->text();
-        $price = $domain->filter('label > span.b-table-tlds__price-wrapper')->text();
-
-        $this->line('|' . $name . '|' . $price . '|');
-      }
+      $this->line(json_decode($result, true)['domains'][0]['price']);
     }
 }
