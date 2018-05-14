2d08a56677d8a91d5983d361cb13c7b31085d908
Яковлев Алексей <yacovlef@gmail.com>
2018-05-14 22:03:43 +0300

    Доработан механизм обновления цен

--- app/Console/Commands/PriceUpdate.php
+++ app/Console/Commands/PriceUpdate.php
@@ -42,204 +42,243 @@ class PriceUpdate extends Command
      */
     public function handle()
     {
+      $this->reg();
+      $this->rucenter();
+      $this->masterhost();
+      $this->goDaddy();
+      $this->hostinger();
+      $this->jino();
+      $this->fozzy();
+    }
       // REG
       //------------------------------------------------------------------------
-      // $prices = Registrar::findOrFail(1)->prices()->get();
-      //
-      // foreach ($prices as $price) {
-      //   $domainZone =  mb_strtolower($price->domain->name);
-      //   $domainName = (($domainZone == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $domainZone;
-      //   $method = 'POST';
-      //   $registarRequest = 'https://www.reg.ru/domain/new/check_queue';
-      //   $registarRequestQuery = [
-      //     'ru' => 1,
-      //     'domains' => $domainName,
-      //   ];
-      //
-      //   do {
-      //     $domainPrice = $this->getDomainPrice($method, $registarRequest, $registarRequestQuery)['domains'][0]['price'] ?? null;
-      //   } while (!$domainPrice);
-      //
-      //   if ($domainPrice != $price->price) {
-      //     $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $domainZone . ' | NEW: ' . $domainPrice . ' | OLD: ' . $price->price);
-      //   }
-      // }
+        public function reg()
+        {
+
+        $prices = Registrar::findOrFail(1)->prices()->get();
+
+        foreach ($prices as $price) {
+          $tld = mb_strtolower($price->domain->name);
+          $tldName = (($tld == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $tld;
+
+          $method = 'POST';
+          $uri = 'https://www.reg.ru/domain/new/check_queue';
+          $type = 'query';
+          $query = [
+            'ru' => 1,
+            'domains' => $tldName,
+          ];
+
+          do {
+            $priceExt = $this->_getDomainPrice($method, $uri, $type, $query)['domains'][0]['price'] ?? null;
+          } while (!$priceExt);
+
+          if ($priceExt != $price->price) {
+            $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $priceExt . ' | OLD: ' . $price->price);
+          }
+        }
+      }
       //------------------------------------------------------------------------
 
       // RUcenter
       //------------------------------------------------------------------------
-      // $prices = Registrar::findOrFail(2)->prices()->get();
-      //
-      // foreach ($prices as $price) {
-      //   $domainZone =  mb_strtolower($price->domain->name);
-      //   $domainName = (($domainZone == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $domainZone;
-      //   $method = 'POST';
-      //   $registarRequest = 'https://www.nic.ru/app/v1/get/services';
-      //   $registarRequestQuery = [
-      //     'lang' => 'ru',
-      //     'limit' => 1,
-      //     'offset' => 0,
-      //     'search' => $domainName,
-      //     'url' => 'domains',
-      //   ];
-      //
-      //   do {
-      //     $domainPrice = $this->getDomainPrice($method, $registarRequest, $registarRequestQuery)['body']['services'][0]['prices'][0]['cost']['value'] ?? null;
-      //   } while (!$domainPrice);
-      //
-      //   if ($domainPrice != $price->price) {
-      //     $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $domainZone . ' | NEW: ' . $domainPrice . ' | OLD: ' . $price->price);
-      //   }
-      // }
+      public function rucenter()
+      {
+        $prices = Registrar::findOrFail(2)->prices()->get();
+
+        foreach ($prices as $price) {
+          $tld =  mb_strtolower($price->domain->name);
+          $tldName = (($tld == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $tld;
+
+          $method = 'POST';
+          $uri = 'https://www.nic.ru/app/v1/get/services';
+          $type = 'query';
+          $query = [
+            'lang' => 'ru',
+            'limit' => 1,
+            'offset' => 0,
+            'search' => $tldName,
+            'url' => 'domains',
+          ];
+
+          do {
+            $priceExt = $this->_getDomainPrice($method, $uri, $type, $query)['body']['services'][0]['prices'][0]['cost']['value'] ?? null;
+          } while (!$priceExt);
+
+          if ($priceExt != $price->price) {
+            $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $priceExt . ' | OLD: ' . $price->price);
+          }
+        }
+      }
       //------------------------------------------------------------------------
 
       // .masterhost
       //------------------------------------------------------------------------
-      // $method = 'POST';
-      // $registarRequest = 'https://masterhost.ru/rs/products/list.json';
-      // $registarRequestQuery = null;
-      //
-      // do {
-      //   $products = $this->getDomainPrice($method, $registarRequest, $registarRequestQuery)['products'] ?? null;
-      // } while (!$products);
-      //
-      // foreach ($products as $product) {
-      //   if ($product['category'] != 'domain') continue;
-      //
-      //   $domain = '.' . $product['properties'][0]['value'];
-      //   $price = (int) $product['unitPrice'];
-      //
-      //   $registrarMasterhostPrices[$domain] = $price;
-      // }
-      //
-      // $prices = Registrar::findOrFail(3)->prices()->get();
-      //
-      // foreach ($prices as $price) {
-      //   $domainZone =  mb_strtolower($price->domain->name);
-      //   $domainName = (($domainZone == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $domainZone;
-      //
-      //   if ($registrarMasterhostPrices[$domainZone] != $price->price) {
-      //     $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $domainZone . ' | NEW: ' . $registrarMasterhostPrices[$domainZone] . ' | OLD: ' . $price->price);
-      //   }
-      // }
+      public function masterhost()
+      {
+        $method = 'POST';
+        $uri = 'https://masterhost.ru/rs/products/list.json';
+        $type = 'query';
+        $query = null;
+
+        do {
+          $domainsExt = $this->_getDomainPrice($method, $uri, $type, $query)['products'] ?? null;
+        } while (!$domainsExt);
+
+        foreach ($domainsExt as $domainExt) {
+          if ($domainExt['category'] != 'domain')
+            continue;
+
+          $tldExt = '.' . $domainExt['properties'][0]['value'];
+          $priceExt = (int) $domainExt['unitPrice'];
+
+          $pricesExt[$tldExt] = $priceExt;
+        }
+
+        $prices = Registrar::findOrFail(3)->prices()->get();
+
+        foreach ($prices as $price) {
+          $tld =  mb_strtolower($price->domain->name);
+          $tldName = (($tld == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $tld;
+
+          if ($pricesExt[$tld] != $price->price) {
+            $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $pricesExt[$tld] . ' | OLD: ' . $price->price);
+          }
+        }
+      }
       //------------------------------------------------------------------------
 
       // GoDaddy
       //------------------------------------------------------------------------
-      // $prices = Registrar::findOrFail(5)->prices()->get();
-      //
-      // foreach ($prices as $price) {
-      //   $domainZone =  mb_strtolower($price->domain->name);
-      //   $domainName = (($domainZone == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $domainZone;
-      //   $method = 'GET';
-      //   $registarRequest = 'https://ru.godaddy.com/domainsapi/v1/search/exact';
-      //   $registarRequestQuery = [
-      //     'q' => $domainName,
-      //   ];
-      //
-      //   do {
-      //     $domainPrice = (int) $this->getDomainPrice($method, $registarRequest, $registarRequestQuery)['Products'][0]['PriceInfo']['CurrentPrice'];
-      //   } while (!$domainPrice);
-      //
-      //   if ($domainPrice != $price->price) {
-      //     $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $domainZone . ' | NEW: ' . $domainPrice . ' | OLD: ' . $price->price);
-      //   }
-      // }
+      public function goDaddy()
+      {
+        $prices = Registrar::findOrFail(5)->prices()->get();
+
+        foreach ($prices as $price) {
+          $tld =  mb_strtolower($price->domain->name);
+          $tldName = (($tld == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $tld;
+
+          $method = 'GET';
+          $uri = 'https://ru.godaddy.com/domainsapi/v1/search/exact';
+          $type = 'query';
+          $query = [
+            'q' => $tldName,
+          ];
+
+          do {
+            $priceExt = (int) $this->_getDomainPrice($method, $uri, $type, $query)['Products'][0]['PriceInfo']['CurrentPrice'];
+          } while (!$priceExt);
+
+          if ($priceExt != $price->price) {
+            $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $priceExt . ' | OLD: ' . $price->price);
+          }
+        }
+      }
       //------------------------------------------------------------------------
 
       // HOSTINGER
       //------------------------------------------------------------------------
-      $method = 'POST';
-      $registarRequest = 'https://www.hostinger.ru/domain-search';
-      $registarRequestQuery = [
-        'q' => 'parcedomainprice',
-      ];
+      public function hostinger()
+      {
+        $method = 'POST';
+        $uri = 'https://www.hostinger.ru/domain-search';
+        $type = 'form_params';
+        $query = [
+          'q' => 'parcedomainprice',
+        ];
 
-      do {
-        $domains = $this->getDomainPrice($method, $registarRequest, $registarRequestQuery) ?? null;
-      } while (!$domains);
+        do {
+          $domainsExt = $this->_getDomainPrice($method, $uri, $type, $query) ?? null;
+        } while (!$domainsExt);
 
-      foreach ($domains as $domain) {
+        foreach ($domainsExt as $domainExt) {
+          $tldExt = $domainExt['product']['config']['tld'];
+          $priceExt = (int) $domainExt['product']['billing']['price'];
 
-        $tld = $domain['product']['config']['tld'];
-        $price = (int) $domain['product']['billing']['price'];
+          $pricesExt[$tldExt] = $priceExt;
+        }
 
-        $registrarHostingerPrices[$tld] = $price;
-      }
+        $prices = Registrar::findOrFail(6)->prices()->get();
 
-      $prices = Registrar::findOrFail(6)->prices()->get();
+        foreach ($prices as $price) {
+          $tld =  mb_strtolower($price->domain->name);
 
-      foreach ($prices as $price) {
-        $tld =  mb_strtolower($price->domain->name);
-
-        if ($registrarHostingerPrices[$tld] != $price->price) {
-          $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $registrarHostingerPrices[$tld] . ' | OLD: ' . $price->price);
+          if ($pricesExt[$tld] != $price->price) {
+            $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $pricesExt[$tld] . ' | OLD: ' . $price->price);
+          }
         }
       }
       //------------------------------------------------------------------------
 
       // ДЖИНО
       //------------------------------------------------------------------------
-      // $html = file_get_contents('https://domains.jino.ru/price/');
-      //
-      // $crawler = new Crawler($html);
-      //
-      // $nodeValues = $crawler->filter('#domains-price > li')->each(function (Crawler $node) {
-      //   return $node;
-      // });
-      //
-      // foreach ($nodeValues as $value) {
-      //   $domain = '.' . substr($value->filter('a > h3')->text(), 4);
-      //   $price = $value->filter('a > div.domains-price-prices > div.domains-price-regprice > span > span.rub-value')->text();
-      //
-      //   $registrarJinoPrices[$domain] = $price;
-      // }
-      //
-      // $prices = Registrar::findOrFail(7)->prices()->get();
-      //
-      // foreach ($prices as $price) {
-      //   $domainZone =  mb_strtolower($price->domain->name);
-      //   $domainName = (($domainZone == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $domainZone;
-      //
-      //   if ($registrarJinoPrices[$domainZone] != $price->price) {
-      //     $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $domainZone . ' | NEW: ' . $registrarJinoPrices[$domainZone] . ' | OLD: ' . $price->price);
-      //   }
-      // }
+      public function jino()
+      {
+        $html = file_get_contents('https://domains.jino.ru/price/');
+
+        $crawler = new Crawler($html);
+
+        $nodes = $crawler->filter('#domains-price > li')->each(function (Crawler $node) {
+          return $node;
+        });
+
+        foreach ($nodes as $node) {
+          $tldExt = '.' . substr($node->filter('a > h3')->text(), 4);
+          $priceExt = $node->filter('a > div.domains-price-prices > div.domains-price-regprice > span > span.rub-value')->text();
+
+          $pricesExt[$tldExt] = $priceExt;
+        }
+
+        $prices = Registrar::findOrFail(7)->prices()->get();
+
+        foreach ($prices as $price) {
+          $tld =  mb_strtolower($price->domain->name);
+          $tldName = (($tld == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $tld;
+
+          if ($pricesExt[$tld] != $price->price) {
+            $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $pricesExt[$tld] . ' | OLD: ' . $price->price);
+          }
+        }
+      }
       //------------------------------------------------------------------------
 
       // FOZZY
       //------------------------------------------------------------------------
-      // $prices = Registrar::findOrFail(8)->prices()->get();
-      //
-      // foreach ($prices as $price) {
-      //   $domainZone =  mb_strtolower($price->domain->name);
-      //   $domainName = (($domainZone == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $domainZone;
-      //   $method = 'GET';
-      //   $registarRequest = 'https://accounts.fozzy.com/tools/api/prices/domains/' . $domainName;
-      //   $registarRequestQuery = [
-      //     'locale' => 'ru',
-      //     'actiontype' => 'register',
-      //     'currency' => 'rub',
-      //   ];
-      //
-      //   do {
-      //     $domainPrice = (int) $this->getDomainPrice($method, $registarRequest, $registarRequestQuery)['data']['attributes']['prices'][0]['price'];
-      //   } while (!$domainPrice);
-      //
-      //   if ($domainPrice != $price->price) {
-      //     $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $domainZone . ' | NEW: ' . $domainPrice . ' | OLD: ' . $price->price);
-      //   }
-      // }
+      public function fozzy()
+      {
+        $prices = Registrar::findOrFail(8)->prices()->get();
+
+        foreach ($prices as $price) {
+          $tld =  mb_strtolower($price->domain->name);
+          $tldName = (($tld == '.рф') ? 'парсингдоменценац' : 'parcedomainpricew') . $tld;
+
+          $method = 'GET';
+          $uri = 'https://accounts.fozzy.com/tools/api/prices/domains/' . $tldName;
+          $type = 'query';
+          $query = [
+            'locale' => 'ru',
+            'actiontype' => 'register',
+            'currency' => 'rub',
+          ];
+
+          do {
+            $priceExt = (int) $this->_getDomainPrice($method, $uri, $type, $query)['data']['attributes']['prices'][0]['price'];
+          } while (!$priceExt);
+
+          if ($priceExt != $price->price) {
+            $this->info('| Регистратор: ' . $price->registrar->name . ' | Домен: ' . $tld . ' | NEW: ' . $priceExt . ' | OLD: ' . $price->price);
+          }
+        }
+      }
       //------------------------------------------------------------------------
-    }
 
 
-    public function getDomainPrice($method, $registarRequest, $registarRequestQuery)
+    public function _getDomainPrice($method, $uri, $type, $query)
     {
       $client = new Client();
 
-      $result = $client->request($method, $registarRequest, [
-        'form_params' => $registarRequestQuery,
+      $result = $client->request($method, $uri, [
+        $type => $query,
       ])->getBody();
 
       return json_decode($result, true);
